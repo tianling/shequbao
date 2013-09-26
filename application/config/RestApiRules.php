b21b66c3696efeb3b34d405ae4570d0afe592462
lancelot <lancelot1215@gmail.com>
2013-09-26 23:15:07 +0800

    xx

--- application/config/RestApiRules.php
+++ application/config/RestApiRules.php
@@ -38,7 +38,7 @@ return array(
 				'verb'=>'PUT'
 		),
 		array(
-				'pattern'=>'<_m:(user|friends)>/<resourceId:\d+>:<_a:(Address|Bind|SayHelloToMe|RandomFriends|Trends|MyTrends|FriendsTrends|OfflineMessage)>',
+				'pattern'=>'<_m:(user|friends)>/<resourceId:\d+>:<_a:(Address|Bind|SayHelloToMe|RandomFriends|Trends|FriendsTrends|OfflineMessage)>',
 				'<_m>/service/get<_a>',
 				'verb'=>'GET'
 		),
