1165727cafb9cd427a10fa44fd3edf2334b722bb
lancelot <lancelot1215@gmail.com>
2013-10-05 18:21:59 +0800

    x

--- application/modules/user/controllers/service/createUserAddressAction.php
+++ application/modules/user/controllers/service/createUserAddressAction.php
@@ -13,12 +13,34 @@ class createUserAddressAction extends CmsAction{
 			$address->attributes = $newAttributes;
 			if($address->save()){
 				if ( isset($newAttributes['community']) ){
+					$cId = $newAttributes['community'];
 					$cu = new CommunityUser();
 					$cu->attributes = array(
-							'community_id' => $newAttributes['community'],
+							'community_id' => $cId,
 							'user_id' => $resourceId
 					);
 					$cu->save();
+					
+					$room = ChatRoom::model()->find('community_id=:c',array(':c'=>$cId));
+					if ( $room === null ){
+						$community = Community::model()->findByPk($cId,array('select'=>'community_name'));
+						$room = new ChatRoom();
+						$room->attributes = array(
+								'community_id' => $cId,
+								'room_name' => $community->community_name,
+								'user_num' => 1
+						);
+					}else {
+						++$room->user_num;
+					}
+					$room->save();
+					
+					$ownedRoom = new UserOwnedChat();
+					$ownedRoom->attributes = array(
+							'room_id' => $room->primaryKey,
+							'user_id' => $resourceId
+					);
+					$ownedRoom->save();
 				}
 				
 				$this->response(200,'添加地址成功');
